2ecef63f892365449e79c5027cf3394ab7f56171
Buddha <buddhagautam231@gmail.com>
2023-03-18 20:15:06 +0545

    migrating my vim configs to lua

---
+++ .config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,6 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '?', ":Telescope current_buffer_fuzzy_find sorting_strategy=ascending<CR>", {})
+vim.keymap.set('n', '<C-l>', builtin.find_files, {})
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
---
+++ .config/nvim/lua/buddha/plugins.lua
@@ -0,0 +1,68 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use ('wbthomason/packer.nvim')
+  use 'francoiscabrol/ranger.vim'
+  use {'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use ('mbbill/undotree')
+  -- use {
+  --     'itchyny/lightline.vim',
+  -- }
+
+  use 'mhinz/vim-startify'
+  -- use 'vim-airline/vim-airline'
+
+  use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use 'terrortylor/nvim-comment'
+
+  use {
+      "olimorris/onedarkpro.nvim",
+      as = 'onedark-vivid',
+      config = function()
+          vim.cmd('colorscheme onedark_vivid')
+      end
+  }
+
+  use {
+	  'nvim-treesitter/nvim-treesitter',
+	  run = function()
+		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+		  ts_update()
+	  end,
+
+  }
+
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v1.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},           -- Optional
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},         -- Required
+    {'hrsh7th/cmp-nvim-lsp'},     -- Required
+    {'hrsh7th/cmp-buffer'},       -- Optional
+    {'hrsh7th/cmp-path'},         -- Optional
+    {'saadparwaiz1/cmp_luasnip'}, -- Optional
+    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'},             -- Required
+    {'rafamadriz/friendly-snippets'}, -- Optional
+  }
+}
+
+  end)
