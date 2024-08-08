76d28315e7db5e0247c6fd459f272e38581acc0d
buddhadonthavemoney <buddhamani.gautam@khalti.com>
2024-08-08 21:32:52 +0545

    nvim config sync

--- .config/nvim/after/plugin/nvimtree.lua
+++ .config/nvim/after/plugin/nvimtree.lua
@@ -6,10 +6,11 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+local nvim_tree = require("nvim-tree")
+nvim_tree.setup()
 
 -- OR setup with some options
-require("nvim-tree").setup({
+nvim_tree.setup({
     sort_by = "case_sensitive",
     view = {
         width = 30,
@@ -19,8 +20,18 @@ require("nvim-tree").setup({
     },
     filters = {
         dotfiles = true,
+        -- dotfiles = false,
+        git_clean = false,
+        no_buffer = false,
+        custom = { 'node_modules', '__pycache__' },
     },
 })
 
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { silent = true })
 vim.keymap.set("n", "<C-f>", ":NvimTreeFindFile<CR>", { silent = true })
+
+nvim_tree.setup({
+    filters = {
+    },
+(...)
+})
